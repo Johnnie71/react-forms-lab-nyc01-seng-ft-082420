a55fc063d36a248d1f33c803b419391eaaea1cfa
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-28 21:12:08 -0400

    Counter is working

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
+
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      input: ''
+    };
   }
+
+  changeHandler = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  remaining() {
+     return (this.props.maxChars - this.state.input.length)
+  } 
+    
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.changeHandler(event)} value={this.state.input} />
+        <strong>{this.remaining()}</strong>
       </div>
     );
   }
